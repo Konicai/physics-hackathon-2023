a3e04d7b6231e6ae70f78ecedb1122a507439746
Stillmahn <milesethanstillie@gmail.com>
2023-11-04 13:29:09 -0400

    add mirror scene

--- Main.gd
+++ Main.gd
@@ -1,9 +1,25 @@
 extends Node
 
-@export var Ball: PackedScene
+@export var Ball: PackedScene = preload("res://Ball.tscn")
+@export var Mirror: PackedScene = preload("res://Mirror.tscn")
+
 
 func _input(event):
 	if event.is_action_pressed("click"):
 		var new_ball = Ball.instantiate()
 		new_ball.position = get_viewport().get_mouse_position()
+		print("sakdfjh")
 		add_child(new_ball)
+		
+	if event.is_action_pressed("mirror"):
+		makeMirror()
+
+
+
+
+
+_makeMirror():
+	var new_mirror = Mirror.instantiate()
+		new_mirror.position = get_viewport().get_mouse_position()
+		print("bruh")
+		add_child(new_mirror)
