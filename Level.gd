083ae1e0dd7a80bfe0f9649fff8396fafaabfb4a
Jack Rubio <rubiojack123@gmail.com>
2023-11-04 18:03:31 -0400

    mirror added, not working
    words

--- Level.gd
+++ Level.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 var ball_scene = preload("res://Photon.tscn")
-var launch_speed = Vector2(0, -500)
+var launch_speed = Vector2(-500, 0)
 var ball_to_launch = null
 var ball_fired = false
 var last_ball_position = Vector2()
@@ -9,12 +9,19 @@ var last_ball_position = Vector2()
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var screen_size = get_viewport_rect().size
-	$StaticBody2D.position = screen_size / 2
+	$laser.position = Vector2(screen_size.x / 2, screen_size.y - 200)
+	$topWall.position = Vector2(screen_size.x / 2, 0)
+	$bottomWall.position = Vector2(screen_size.x / 2, screen_size.y)
+	$rightWall.position = Vector2(screen_size.x, screen_size.y / 2)
+	$leftWall.position = Vector2(0, screen_size.y / 2)
+	
+	
 	# Assuming 'Line2D' is already a node in the scene
 	$Line2D.width = 2
 	$Line2D.default_color = Color(1, 0, 0)  # Set line color to red
+	
 func _process(delta):
-	if Input.is_action_just_pressed("fire"):
+	if Input.is_action_just_pressed("fire") and not ball_fired:
 		fire_ball()
 	if $Line2D.get_point_count() > 0:
 		$Line2D.set_point_position($Line2D.get_point_count() - 1, $Ball.position)
@@ -23,6 +30,8 @@ func _process(delta):
 		_on_Ball_position_changed(ball_to_launch.position)
 		# Update the last position to the current one
 		last_ball_position = ball_to_launch.position
+
+
 func _physics_process(delta):
 	if ball_to_launch and not ball_fired:
 		ball_to_launch.apply_impulse(launch_speed)
@@ -30,15 +39,18 @@ func _physics_process(delta):
 		# Clear previous points when a new ball is launched
 		$Line2D.points.clear()
 
+
 func fire_ball():
 	var ball_instance = ball_scene.instantiate()
 	ball_instance.sleeping = false
-	ball_instance.position = $StaticBody2D.global_position + Vector2(0, -100)
+	ball_instance.position = $laser.global_position + Vector2(0, -15)
+	
 	add_child(ball_instance)
 	ball_to_launch = ball_instance
 	last_ball_position = ball_to_launch.position
 	# Connect 'position_changed' signal to the method to update the line
 	# ball_instance.connect("position_changed", Callable(self, "_on_Ball_position_changed"))
+
 
 # This method will be called when the 'position_changed' signal is emitted
 func _on_Ball_position_changed(new_position):
