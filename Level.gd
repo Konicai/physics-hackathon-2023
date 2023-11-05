dee0d49dfc4b2dcaab34fe528886e49887a9c78b
Stillmahn <milesethanstillie@gmail.com>
2023-11-04 21:52:15 -0400

    Added label to track remaining attempts
    Will need to review global variables, as it won't be tracked currently through scene reset
    
    (cherry picked from commit 7bfc2dfcd160d3b73455a2513c989b2fad28bc0d)

--- Level.gd
+++ Level.gd
@@ -5,7 +5,7 @@ var launch_speed = Vector2(-1500, 0)
 var ball_to_launch = null
 var ball_fired = false
 var last_ball_position = Vector2()
-
+var tries = 3
 var mirror_scene = preload("res://Mirror.tscn")
 var planet = preload("res://Grav.tscn")
 var receptor_scene = preload("res://Receptor.tscn")
@@ -23,7 +23,7 @@ var lost_round = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
+
 	var screen_size = get_viewport_rect().size
 	var receptor = receptor_scene.instantiate()
 	receptor.position = Vector2(50,50)
@@ -34,22 +34,25 @@ func _ready():
 	mirror1.position = Vector2(screen_size.x / 2, screen_size.y / 2)
 	mirror1.rotate(PI / 4)
 	add_child(mirror1)
-	
+
 	planett = planet.instantiate()
 	planett.position = Vector2(screen_size.x - 500, screen_size.y - 500)
 	add_child(planett)
-	
+
 	$laser.position = Vector2(screen_size.x - 100, screen_size.y - 100)
 	$laser.rotate(-PI/2)
 	$topWall.position = Vector2(screen_size.x / 2, 0)
 	$bottomWall.position = Vector2(screen_size.x / 2, screen_size.y)
 	$rightWall.position = Vector2(screen_size.x, screen_size.y / 2)
 	$leftWall.position = Vector2(0, screen_size.y / 2)
-	
+	$UI/TryLabel.position = Vector2(screen_size.x-350, 50)
+
+	set_try_label()
+
 	# Assuming 'Line2D' is already a node in the scene
 	$Line2D.width = 2
 	$Line2D.default_color = Color(1, 0, 0)  # Set line color to red
-	
+
 	
 
 	
@@ -64,7 +67,7 @@ func _process(delta):
 
 
 func _physics_process(delta):
-	if ball_to_launch: 
+	if ball_to_launch:
 		if not ball_fired:
 			ball_to_launch.apply_impulse(launch_speed)
 			ball_fired = true
@@ -74,19 +77,22 @@ func _physics_process(delta):
 			ball_to_launch.set_freeze_enabled(true)
 			won_round = ball_to_launch.didWin()
 			lost_round = ball_to_launch.didLose()
-	
-			
-			
-	
+
+
+
+
 
 
 func fire_ball():
+    reduce_try()
+	set_try_label()
+
 	print("firing, ball fired is: " + str(ball_fired))
 	if ball_fired == false:
 		var ball_instance = ball_scene.instantiate()
 		ball_instance.sleeping = false
 		ball_instance.position = $laser.global_position + Vector2(-15, 0)
-		
+
 		add_child(ball_instance)
 		ball_to_launch = ball_instance
 		last_ball_position = ball_to_launch.position
@@ -105,6 +111,8 @@ func _on_Ball_position_changed(new_position):
 	# Add the new position of the ball to the Line2D points
 	$Line2D.add_point(new_position)
 	
+func set_try_label() -> void:
+	$UI/TryLabel.text = "Tries left: %s" % tries
 
 func _input(event):
 	if event.is_action_pressed("mirror"):
@@ -113,6 +121,10 @@ func _input(event):
 		fire_ball()
 	elif event.is_action_pressed("Obsorber"):
 		fire_ball()
+
+func reduce_try():
+	tries -= 1
+
 
 func _placeObsorber():
 	var new_obsorber = obsorber_scene.instantiate()
